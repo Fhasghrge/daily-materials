956618eaf12ff7524cb5d9b98d365a65d974020b
Fhasghrge <1969533391@qq.com>
2021-04-24 20:25:43 +0800

    feat: two diff way to get data“

--- packages/SearchInput/src/index.jsx
+++ packages/SearchInput/src/index.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Input } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
-import { useObservable } from 'rxjs-hooks'
+import { useObservable, useEventCallback } from 'rxjs-hooks'
+import { debounceTime, filter, mapTo, scan } from 'rxjs/operators'
+
 import { ServiceContext, useInject, useInjector } from './utils/serviceProvider'
 import SearchService from './index.service'
 import "antd/dist/antd.css";
@@ -13,12 +15,30 @@ const SearchInput = () => {
 
   const handlePress = (e) => {
     searchServie.send(e.target.value)
+    handleClick(e.target.value)
   }
+  const [handleClick, results] = useEventCallback(event$ => {
+    return event$.pipe(
+      debounceTime(500),
+      filter(msg => !!msg),
+      mapTo('test'),
+      scan((pre, curr) => [...pre, curr], [])
+    )
+  }, [])
+  
+  useEffect(() => {
+    console.log(results)
+  }, [results])
   return (
     <div>
-      <Input prefix={<SearchOutlined /> } onPressEnter={handlePress}></Input>
+      <Input prefix={<SearchOutlined /> } onChange={handlePress}></Input>
       <ul>
         {res.map((item, index) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+      <ul>
+        {results.map((item, index) => (
           <li key={index}>{item}</li>
         ))}
       </ul>
--- packages/SearchInput/src/index.service.js
+++ packages/SearchInput/src/index.service.js
@@ -1,11 +1,27 @@
-import { BehaviorSubject } from 'rxjs'
-import { filter, scan } from 'rxjs/operators'
+/***
+ * 采用Angular这种视图和逻辑分离的方法
+ * 把 service 分开使用
+ */
+import { BehaviorSubject, from } from 'rxjs'
+import { filter, scan, debounceTime, tap, map, switchAll } from 'rxjs/operators'
+
+const fackPromise = (msg) => {
+  return fetch('https://6084086d9b2bed001704096b.mockapi.io/search/searchNname').then(res => res.json())
+}
 
 class SearchService {
   resultSource$ = new BehaviorSubject();
+
   result$ = this.resultSource$.pipe(
+    debounceTime(500),
     filter(msg => !!msg),
-    scan((pre, curr) => [...pre, curr], [])
+    tap((msg) => console.log(msg)),
+    map(msg => {
+      return from(
+        fackPromise(msg)
+      )
+    }),
+    switchAll(),
   )
   send(msg) {
     this.resultSource$.next(msg)
